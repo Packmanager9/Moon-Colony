b835b700b5bc4a25d8aa54ecfc25935374b5929f
GBmixo <52257096+GBmixo@users.noreply.github.com>
2019-11-12 15:03:46 -0800

    Add files via upload

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-Base.create({name: "Neoterrum", population: 0, happiness: 70, waste_management: 0, power_generation: 0, production: 0, housing: 0, luxury: 0})
+#Base.create({name: "Neoterrum", population: 0, happiness: 70, waste_management: 0, power_generation: 0, production: 0, housing: 0, luxury: 0})
 
 =begin 
 MaterialResource.create({name: "Helium"})
@@ -11,9 +11,10 @@ MaterialResource.create({name: "Organics"})
 MaterialResource.create({name: "Metals"})
 MaterialResource.create({name: "Metalloids"})
 MaterialResource.create({name: "Credits"}) 
+MaterialResource.create({name: "Population"}) 
 =end
 
-MaterialResource.create({name: "Population"}) 
+
 =begin
 Location.create({name: "Earth"})
 Location.create({name: "Moon"})
@@ -21,5 +22,14 @@ Location.create({name: "Ship"})
 Location.create({name: "Space"})
 =end
 
-#MatDistribution.create()
+=begin
+MatDistribution.create({resource_id: 1, location_id: 1, amount: 10000})
+MatDistribution.create({resource_id: 2, location_id: 1, amount: 10000})
+MatDistribution.create({resource_id: 8, location_id: 1, amount: 8000000000})
+MatDistribution.create({resource_id: 3, location_id: 1, amount: 172000000000})
+MatDistribution.create({resource_id: 4, location_id: 1, amount: 560000000000})
+MatDistribution.create({resource_id: 5, location_id: 1, amount: 100000000000000})
+MatDistribution.create({resource_id: 6, location_id: 1, amount: 1000000000000000})
+=end
 
+puts "done"
