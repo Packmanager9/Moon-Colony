d850e20093c6f0760edd74920881a15c530173bf
GBmixo <52257096+GBmixo@users.noreply.github.com>
2019-11-13 13:57:25 -0800

    Add files via upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-    resources :bases, only: [:index, :show]
-    resources :mat_distributions, only: [:index, :show]
-    resources :locations, only: [:index]
-    resources :material_resources, only: [:index]
-    
+  resources :bases, only: [:index, :show]
+  resources :mat_distributions, only: [:index, :show]
+  resources :locations, only: [:index, :show]
+  resources :material_resources, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
