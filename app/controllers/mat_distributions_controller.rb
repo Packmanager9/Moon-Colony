2226bad0c61c75530c7f7e84a4c379c0032060b0
GBmixo <52257096+GBmixo@users.noreply.github.com>
2019-11-13 16:56:22 -0800

    Add files via upload

--- app/controllers/mat_distributions_controller.rb
+++ app/controllers/mat_distributions_controller.rb
@@ -1,11 +1,23 @@
 class MatDistributionsController < ApplicationController
-  def index
-    @mat_dists = MatDistribution.all
-    render json: @mat_dists
-  end
 
-  def show
-    @mat_dist = MatDistribution.find(params[:id])
-    render json: @mat_dist
-  end
+    def set_param
+        params.require(:mat_distribution).permit(:amount)
+    end
+
+    def index
+        @mat_dists = MatDistribution.all
+        render json: @mat_dists
+    end
+
+    def show
+        @mat_dist = MatDistribution.find(params[:id])
+        render json: @mat_dist
+    end
+
+    def update
+        mat_dist = MatDistribution.find(params[:id])
+
+        mat_dist.update(set_param)
+        render :json => mat_dist
+    end
 end
