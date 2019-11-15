861ed936e047e8b24100d7808988e75a40d6815a
Packmanager9 <36295363+Packmanager9@users.noreply.github.com>
2019-11-15 14:02:52 -0800

    Update data_interface.js

--- frontend/data_interface.js
+++ frontend/data_interface.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const locationsURL = 'http://localhost:3000/locations'
     const materialsURL = 'http://localhost:3000/material_resources'
     const matDistributionsURL = 'http://localhost:3000/mat_distributions'
-    const basesURL = 'http://localhost:3000/bases'
+    const basesURL = 'http://localhost:3000/colonies'
 
     fetchLocations(locationsURL);
     fetchMaterials(materialsURL);
@@ -40,6 +40,35 @@ function fetchMatDistributions(url){
     })
 }
 
+function updateBase(url, baseId, hash){
+    //let column = hash[key]
+    //`${url}/${matDistId}`
+    console.log('updating')
+    fetch('http://localhost:3000/colonies/1', {
+        method: "PATCH", 
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify({
+             name: "NeW mOoN",
+             population: hash.population,
+             happiness: hash.happiness,
+             waste_management: hash.waste_management,
+             solar_power: hash.solar_power,
+             nuclear_power: hash.nuclear_power,
+             material_production: hash.material_production,
+             food_production: hash.food_production,
+             housing: hash.housing,
+             luxury: hash.luxury,
+             misc: hash.misc
+        })
+    })
+    .then(resp => resp.json())
+    .then(json => console.log(json))
+    console.log('after fetch')
+}
+
 function fetchBases(url){
     fetch(url)
     .then(resp => {
@@ -51,6 +80,28 @@ function fetchBases(url){
     })
 }
 
+
+function createLocation(locationsUrl, locationName)
+{
+    fetch(`${locationsUrl}`, {
+        method: "POST", 
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify({
+            name: locationName
+        })
+    })
+    .then(resp => resp.json())
+    .then(json => console.log(json))
+}
+function deleteLocation(locationsUrl, locationId)
+{
+    fetch(`${locationsUrl}/${locationId}`, {
+        method: "DELETE"
+    })
+}
 //Pass in the url and a hash with the key being the column and the value being the data
 //EXAMPLE: updateResource(url, 9, 10);
 function updateResource(url, matDistId, matCount){
